72a77c4337f3d0547a708a79a7dcc23085e4b62d
John Young <john.young@foxmail.com>
2026-09-22 12:28:33 +0800

    feat: update README.md and add new Postman collection

--- src/constants/app.py
+++ src/constants/app.py
@@ -4,4 +4,4 @@ from __future__ import annotations
 
 APP_NAME: str = "汉阳（HanYang）"
 APP_VERSION: str = "0.1.0"
-APP_DESCRIPTION: str = "一个基于 DDD 架构的 FastAPI 生产级 Python Web 项目"
+APP_DESCRIPTION: str = "一个基于领域驱动设计（DDD）思想，使用 FastAPI 实现的生产级 Python Web 应用框架"
